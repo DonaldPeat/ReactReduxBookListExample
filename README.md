661344b9a66a0c734ce25cd95f5c5557fe8afbd4
Don Peat <don@donaldpeat.com>
2018-01-08 20:40:09 -0800

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+ # ReactReduxBookListExample
---
+++ src/reducers/books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Antifragile',  pages: 101 },
+    { title: 'All Quiet On The Western Front', pages: 15 },
+    { title: 'The God Delusion', pages: 56 },
+    { title: 'The Average American Male', pages: 26}
+  ]
+}
